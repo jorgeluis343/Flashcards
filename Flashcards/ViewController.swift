6cf5206f128efddef89df0d4872f384aa2a0947b
Jorge Guerrero <jorgeguerrero@jorges-mbp.home>
2018-10-13 16:56:18 -0400

    Initial Commit

---
+++ Flashcards/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flashcards
+//
+//  Created by Jorge Guerrero on 10/13/18.
+//  Copyright © 2018 Jorge Guerrero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
